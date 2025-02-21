1ec54a35156ef22f57a6d87828bdfd8342d41c4d
Kai Yao <kai.yao@ed.ac.uk>
2025-02-21 14:19:31 +0000

    mod

--- launch_ablation_jobs.sh
+++ launch_ablation_jobs.sh
@@ -11,8 +11,14 @@ for i in "${!DELTA_VALUES[@]}"; do
   INDEX=$i
 
   # Create PVC
-  envsubst '$USER $INDEX $STORAGE' < pvc-ablation.yml | kubectl create -f -
+  PVC_YAML=$(envsubst '$USER $INDEX $STORAGE' < pvc-ablation.yml)
+  echo "Creating PVC for INDEX=$INDEX"
+  echo "$PVC_YAML"
+  echo "$PVC_YAML" | kubectl create -f -
 
   # Create Job
-  envsubst '$USER $INFK8S_QUEUE_NAME $INDEX $MAX_DELTA' < job-ablation.yaml | kubectl create -f -
+  JOB_YAML=$(envsubst '$USER $INFK8S_QUEUE_NAME $INDEX $MAX_DELTA' < job-ablation.yaml)
+  echo "Creating Job for INDEX=$INDEX"
+  echo "$JOB_YAML"
+  echo "$JOB_YAML" | kubectl create -f -
 done
