6df0d508fca0181c7def759a27dcb4b08f8a64cc
Kai Yao <kai.yao@ed.ac.uk>
2025-02-21 14:29:39 +0000

    mod

--- launch_ablation_jobs.sh
+++ launch_ablation_jobs.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
-set -xe
+set -xe 
 
-USER="s2470447-infk8s"
-INFK8S_QUEUE_NAME="informatics-user-queue"
-STORAGE="250Gi"
+export STORAGE="250Gi"  # Export STORAGE
 DELTA_VALUES=("0.2")
 
 for i in "${!DELTA_VALUES[@]}"; do
-    MAX_DELTA=${DELTA_VALUES[$i]}
-    INDEX=$i
+  export MAX_DELTA=${DELTA_VALUES[$i]}  # Export MAX_DELTA
+  export INDEX=$i  # Export INDEX
 
-    # Create temporary files for PVC and Job
-    PVC_TEMP_FILE=$(mktemp)
-    JOB_TEMP_FILE=$(mktemp)
+  # Create PVC
+  envsubst '$USER $INDEX $STORAGE' < pvc-ablation.yml | kubectl create -f -
 
-    # Substitute variables in PVC template
-    sed "s/\$USER/$USER/g; s/\$INDEX/$INDEX/g; s/\$STORAGE/$STORAGE/g" pvc-ablation.yml > "$PVC_TEMP_FILE"
-
-    # Substitute variables in Job template
-    sed "s/\$USER/$USER/g; s/\$INFK8S_QUEUE_NAME/$INFK8S_QUEUE_NAME/g; s/\$INDEX/$INDEX/g; s/\$MAX_DELTA/$MAX_DELTA/g" job-ablation.yaml > "$JOB_TEMP_FILE"
-
-    # Create PVC
-    kubectl create -f "$PVC_TEMP_FILE"
-
-    # Create Job
-    kubectl create -f "$JOB_TEMP_FILE"
-
-    # Clean up temporary files
-    rm -f "$PVC_TEMP_FILE" "$JOB_TEMP_FILE"
+  # Create Job
+  envsubst '$USER $INFK8S_QUEUE_NAME $INDEX $MAX_DELTA' < job-ablation.yaml | kubectl create -f -
 done
