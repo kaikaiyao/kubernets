060c59e48d8bebe3c5fd3e0aa29505ba82ab9912
Kai Yao <kai.yao@ed.ac.uk>
2025-02-21 17:56:09 +0000

    mod

--- launch_ablation_jobs.sh
+++ launch_ablation_jobs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -xe 
 
-export STORAGE="50Gi"  # Export STORAGE
+export STORAGE="10Gi"  # Export STORAGE
 DELTA_VALUES=("0.01" "0.02" "0.05" "0.1" "0.2" "0.5" "1.0" "2.0")
+DELTA_VALUES=("2.0")
 
 for i in "${!DELTA_VALUES[@]}"; do
   export MAX_DELTA=${DELTA_VALUES[$i]}  # Export MAX_DELTA
