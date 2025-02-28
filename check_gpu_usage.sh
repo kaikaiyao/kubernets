aef56ae751a4bd4c8ea37277cbca3720cd424eac
Kai Yao <kai.yao@ed.ac.uk>
2025-02-28 20:59:44 +0000

    mod

--- check_gpu_usage.sh
+++ check_gpu_usage.sh
@@ -1,46 +1,62 @@
 #!/bin/bash
 
-# Process the output of kubectl describe pods to extract GPU usage per user
 kubectl describe pods | awk '
-# When a new pod description starts
-/^Name: / {
-    # If the previous pod was running and has a user, output its user and GPU count
-    if (current_pod != "" && is_running && user != "") {
-        print user, gpus
+BEGIN {
+    RS = "";  # Split input into pod blocks separated by blank lines
+    FS = "\n";
+}
+
+{
+    status = "";
+    user = "";
+    gpus = 0;
+    in_limits = 0;
+
+    for (i = 1; i <= NF; i++) {
+        line = $i;
+
+        # Check if the pod is Running
+        if (line ~ /^Status:[[:space:]]+Running/) {
+            status = "Running";
+        }
+
+        # Extract user from Labels
+        if (line ~ /Labels:/) {
+            split(line, lbl_parts, "eidf/user=");
+            if (length(lbl_parts) >= 2) {
+                split(lbl_parts[2], user_part, /[, ]/);
+                user = user_part[1];
+            }
+        }
+
+        # Track if we are in the Limits section
+        if (line ~ /Limits:/) {
+            in_limits = 1;
+        }
+        if (line ~ /Requests:/) {
+            in_limits = 0;
+        }
+
+        # Extract GPU count from Limits
+        if (in_limits && line ~ /nvidia.com\/gpu:/) {
+            split(line, gpu_parts, /nvidia.com\/gpu:[[:space:]]+/);
+            if (length(gpu_parts) >= 2) {
+                gpu_num = gpu_parts[2] + 0;
+                gpus += gpu_num;
+            }
+        }
     }
-    # Reset variables for the new pod
-    current_pod = $2
-    is_running = 0
-    user = ""
-    gpus = 0
+
+    # Accumulate GPUs per user if pod is running and user is found
+    if (status == "Running" && user != "") {
+        total_gpus[user] += gpus;
+    }
 }
 
-# Check if the pod is currently running
-/^Status: Running/ {
-    is_running = 1
-}
-
-# Extract the user from lines containing the eidf/user= label
-/eidf\/user=/ {
-    match($0, /eidf\/user=([^ ]+)/, a)
-    user = a[1]
-}
-
-# Sum GPU requests from lines specifying nvidia.com/gpu under Requests
-/^      nvidia.com\/gpu: / {
-    gpus += $2
-}
-
-# Handle the last pod at the end of input
 END {
-    if (current_pod != "" && is_running && user != "") {
-        print user, gpus
+    # Print results
+    printf "%-20s %s\n", "USER", "GPUs";
+    for (user in total_gpus) {
+        printf "%-20s %d\n", user, total_gpus[user];
     }
-}
-' | awk '{
-    # Sum the GPUs for each user across all their running pods
-    s[$1] += $2
-} END {
-    # Output each user and their total GPU usage
-    for (u in s) print u, s[u]
 }'
