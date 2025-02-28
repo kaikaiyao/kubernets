0c76700d98728c709796b5af91069a2b882ea272
Kai Yao <kai.yao@ed.ac.uk>
2025-02-28 16:47:50 +0000

    mod

--- training/train_model.py
+++ training/train_model.py
@@ -148,7 +148,7 @@ def train_model(
                 f"loss: {loss.item():.4f}, "
                 f"d_k_M_hat.max(): {d_k_M_hat.max().item():.4f}, "
                 f"d_k_M_hat.min(): {d_k_M_hat.min().item():.4f}, "
-                f"d_k_M.max(): {d_k_M.max().item():.4f}"
+                f"d_k_M.max(): {d_k_M.max().item():.4f}, "
                 f"d_k_M.min(): {d_k_M.min().item():.4f}"
             )
 
