76cca620513a5cf7098c000a20c85c110156377b
Kai Yao <kai.yao@ed.ac.uk>
2025-03-04 18:40:23 +0000

    mod

--- models/model_utils.py
+++ models/model_utils.py
@@ -3,7 +3,11 @@ import os
 import pickle
 
 
-def save_finetuned_model(model, path, filename):
+def save_finetuned_model(
+    model, 
+    path, 
+    filename
+):
     model_cpu = copy.deepcopy(model).cpu()
     os.makedirs(path, exist_ok=True)
     save_path = os.path.join(path, filename)
@@ -11,7 +15,9 @@ def save_finetuned_model(model, path, filename):
         pickle.dump(model_cpu, f)
 
 
-def load_finetuned_model(path):
+def load_finetuned_model(
+    path
+):
     with open(path, 'rb') as f:
         model = pickle.load(f)
     return model
